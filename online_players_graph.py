ae6b349a114692b238f43a27946ebb9f7a5cd6ef
SyberiaK <syberiakey@gmail.com>
2026-01-20 20:58:42 +0500

    Reorganize the player graph code

--- online_players_graph.py
+++ online_players_graph.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import json
 import time
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 import requests
@@ -23,19 +24,21 @@ if TYPE_CHECKING:
 
 MINUTE = 60
 MAX_ONLINE_MARKS = (MINUTE // 10) * 24 * 7 * 2  # = 2016 marks - every 10 minutes for the last two weeks
+PLAYER_LIMIT = 2_000_000
 
 setup_logging(config.LOGS_CONFIG_FILE_PATH)
 logger = get_logger(f'{config.NAME}.graph')
 
 scheduler = BlockingScheduler()
 
-cmap = LinearSegmentedColormap.from_list('custom', [(1, 1, 0), (1, 0, 0)], N=100)
-norm = plt.Normalize(0, 2_000_000)
+cmap = LinearSegmentedColormap.from_list('custom', ((1, 1, 0), (1, 0, 0)), N=100)
+norm = plt.Normalize(0, PLAYER_LIMIT)
 mappable = ScalarMappable(norm=norm, cmap=cmap)
 
-ticks = [0, 250000, 500000, 750000, 1000000, 1250000, 1500000, 1750000, 2000000]
-colorbar_ticks_format = FixedFormatter(['0', '250K', '500K', '750K', '1M', '1.25M', '1.5M', '1.75M', '2M+'])
-fig_ticks_format = ['' for _ in ticks]
+y_axis_ticks = tuple(range(0, PLAYER_LIMIT, 250_000))
+# update accordingly to ^
+colorbar_ticks_format = FixedFormatter(('0', '250K', '500K', '750K', '1M', '1.25M', '1.5M', '1.75M', '2M+'))
+fig_ticks_format = tuple('' for _ in y_axis_ticks)
 
 x_major_locator = mdates.DayLocator()
 x_major_formatter = mdates.DateFormatter('%b %d')
@@ -67,94 +70,103 @@ def delete_uploaded_image(host: str, image_key: str) -> bool:
     return response.json().get('success', False)
 
 
+def build_and_save_graph(player_chart_cache_path: Path, gc_cache_path: Path, image_path: Path):
+    old_player_data = pd.read_csv(player_chart_cache_path, parse_dates=['DateTime'])
+
+    marks_count = len(old_player_data.index)
+    if marks_count >= MAX_ONLINE_MARKS:
+        remove_marks = marks_count - MAX_ONLINE_MARKS
+        old_player_data.drop(range(remove_marks + 1), axis=0, inplace=True)
+
+    with open(gc_cache_path, encoding='utf-8') as f:
+        player_count = json.load(f).get('online_players', 0)
+
+    if player_count < 50_000:  # potentially a routine Steam maintenance
+        player_count = old_player_data.iloc[-1]['Players']
+
+    temp_player_data = pd.DataFrame([[f'{utime.utcnow():%Y-%m-%d %H:%M:%S}', player_count]],
+                                    columns=['DateTime', 'Players'])
+
+    new_player_data = pd.concat([old_player_data, temp_player_data])
+
+    new_player_data.to_csv(player_chart_cache_path, index=False)
+
+    fig: plt.Figure
+    ax: plt.Axes
+
+    sns.set_style('whitegrid')
+
+    fig, ax = plt.subplots(figsize=(10, 2.5))
+    ax.scatter('DateTime', 'Players',
+               data=new_player_data,
+               c='Players', cmap=cmap, s=10, norm=norm, linewidths=0.7)
+    ax.fill_between(new_player_data['DateTime'],
+                    new_player_data['Players'] - 20_000,
+                    color=cmap(0.5), alpha=0.4)
+    ax.margins(x=0)
+
+    ax.grid(visible=True, axis='y', linestyle='--', alpha=0.3)
+    ax.grid(visible=False, axis='x')
+    ax.spines['bottom'].set_position('zero')
+    ax.spines['bottom'].set_color('black')
+    ax.set(xlabel='', ylabel='')
+    ax.xaxis.set_ticks_position('bottom')
+    ax.xaxis.set_major_locator(x_major_locator)
+    ax.xaxis.set_major_formatter(x_major_formatter)
+    ax.legend(loc='upper left')
+    ax.text(0.20, 0.88,
+            'Made by @INCS2\n'
+            'updates every 10 min',
+            ha='center', transform=ax.transAxes, color='black', size='8')
+    ax.set_yticks(y_axis_ticks, fig_ticks_format)
+
+    fig.colorbar(mappable, ax=ax,
+                 ticks=y_axis_ticks,
+                 format=colorbar_ticks_format,
+                 pad=0.01)
+
+    fig.subplots_adjust(top=0.933, bottom=0.077, left=0.03, right=1.07)
+
+    fig.savefig(image_path, dpi=200)
+    plt.close()
+
+
+def manage_image_hosting_upload(cache_path: Path, image_path: Path, image_host_domain: str):
+    cache = caching.load_cache(cache_path)
+
+    try:
+        response = delete_uploaded_image(image_host_domain, cache['key'])
+    except (requests.HTTPError, requests.ConnectionError):
+        logger.exception('Caught exception while deleting old graph image from the file uploader!')
+        response = False
+    except KeyError:
+        logger.warning('No deletion key was in the cache file, ignoring the deletion sequence...')
+        response = True
+
+    if not response:
+        logger.error('Failed to delete old graph image from the file uploader!')
+
+    try:
+        with open(image_path, 'rb') as f:
+            response = upload_image_online(f, image_host_domain)
+    except (requests.HTTPError, requests.ConnectionError):
+        logger.exception('Caught exception while uploading graph image to the file uploader!')
+        response = {'link': '', 'key': ''}
+
+    caching.dump_cache(cache_path, response)
+    logger.info('Successfully plotted the player count graph.')
+
+
 @scheduler.scheduled_job('cron', hour='*', minute='0,10,20,30,40,50', second='0')
 def graph_maker():
     # noinspection PyBroadException
     try:
-        old_player_data = pd.read_csv(config.PLAYER_CHART_FILE_PATH, parse_dates=['DateTime'])
-
-        marks_count = len(old_player_data.index)
-        if marks_count >= MAX_ONLINE_MARKS:
-            remove_marks = marks_count - MAX_ONLINE_MARKS
-            old_player_data.drop(range(remove_marks + 1), axis=0, inplace=True)
-
-        with open(config.GC_CACHE_FILE_PATH, encoding='utf-8') as f:
-            player_count = json.load(f).get('online_players', 0)
-
-        if player_count < 50_000:  # potentially Steam maintenance
-            player_count = old_player_data.iloc[-1]['Players']
-
-        temp_player_data = pd.DataFrame(
-            [[f'{utime.utcnow():%Y-%m-%d %H:%M:%S}', player_count]],
-            columns=['DateTime', 'Players'],
-        )
-
-        new_player_data = pd.concat([old_player_data, temp_player_data])
-
-        new_player_data.to_csv(config.PLAYER_CHART_FILE_PATH, index=False)
-
-        fig: plt.Figure
-        ax: plt.Axes
-
-        sns.set_style('whitegrid')
-
-        fig, ax = plt.subplots(figsize=(10, 2.5))
-        ax.scatter('DateTime', 'Players',
-                   data=new_player_data,
-                   c='Players', cmap=cmap, s=10, norm=norm, linewidths=0.7)
-        ax.fill_between(new_player_data['DateTime'],
-                        new_player_data['Players'] - 20_000,
-                        color=cmap(0.5), alpha=0.4)
-        ax.margins(x=0)
-
-        ax.grid(visible=True, axis='y', linestyle='--', alpha=0.3)
-        ax.grid(visible=False, axis='x')
-        ax.spines['bottom'].set_position('zero')
-        ax.spines['bottom'].set_color('black')
-        ax.set(xlabel='', ylabel='')
-        ax.xaxis.set_ticks_position('bottom')
-        ax.xaxis.set_major_locator(x_major_locator)
-        ax.xaxis.set_major_formatter(x_major_formatter)
-        ax.legend(loc='upper left')
-        ax.text(0.20, 0.88,
-                'Made by @INCS2\n'
-                'updates every 10 min',
-                ha='center', transform=ax.transAxes, color='black', size='8')
-        ax.set_yticks(ticks, fig_ticks_format)
-
-        fig.colorbar(mappable, ax=ax,
-                     ticks=ticks,
-                     format=colorbar_ticks_format,
-                     pad=0.01)
-
-        fig.subplots_adjust(top=0.933, bottom=0.077, left=0.03, right=1.07)
-
-        fig.savefig(config.GRAPH_IMG_FILE_PATH, dpi=200)
-        plt.close()
-
-        cache = caching.load_cache(config.GRAPH_CACHE_FILE_PATH)
-
-        try:
-            response = delete_uploaded_image('kappa.lol', cache['key'])
-        except (requests.HTTPError, requests.ConnectionError):
-            logger.exception('Caught exception while deleting old graph image from the file uploader!')
-            response = False
-        except KeyError:
-            logger.warning('No deletion key was in the cache file, ignoring the deletion sequence...')
-            response = True
-
-        if not response:
-            logger.error('Failed to delete old graph image from the file uploader!')
-
-        try:
-            with open(config.GRAPH_IMG_FILE_PATH, 'rb') as f:
-                response = upload_image_online(f, 'kappa.lol')
-        except (requests.HTTPError, requests.ConnectionError):
-            logger.exception('Caught exception while uploading graph image to the file uploader!')
-            response = {'link': '', 'key': ''}
-
-        caching.dump_cache(config.GRAPH_CACHE_FILE_PATH, response)
-        logger.info('Successfully plotted the player count graph.')
+        build_and_save_graph(config.PLAYER_CHART_FILE_PATH,
+                             config.GC_CACHE_FILE_PATH,
+                             config.GRAPH_IMG_FILE_PATH)
+        manage_image_hosting_upload(config.GRAPH_CACHE_FILE_PATH,
+                                    config.GRAPH_IMG_FILE_PATH,
+                                    config.IMAGE_HOSTING_DOMAIN)
     except Exception:
         logger.exception('Caught exception in graph maker!')
         time.sleep(MINUTE)
@@ -163,8 +175,8 @@ def graph_maker():
 
 def main():
     try:
-        scheduler.start()
         logger.info('Started.')
+        scheduler.start()
     except KeyboardInterrupt:
         logger.info('Terminated.')
 
